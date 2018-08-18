33a62ddd88552a3d2bd94490f180a6e58f7115b5
legendrybot <42031779+legendrybot@users.noreply.github.com>
2018-08-18 20:01:36 +0200

    Update mario.js

--- mario.js
+++ mario.js
@@ -292,8 +292,6 @@ const Love = [  "**احبك / عدد قطرات المـــطر والشجر و
 });
 
 
-
-
-//THIS MUST BE THIS WAY
+// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
