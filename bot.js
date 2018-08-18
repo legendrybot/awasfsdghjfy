47f1e2fa870b6186553d4141e53edff0760106ec
legendrybot <42031779+legendrybot@users.noreply.github.com>
2018-08-18 19:17:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -301,54 +301,6 @@ const Love = [  "**احبك / عدد قطرات المـــطر والشجر و
 
 
 
-
-
-client.on('message', message => {
-     var prefix = "$"
-     if (message.author.bot) return;
-if (message.content.startsWith(prefix + "uptime")) {
-    let uptime = client.uptime;
-
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
-
-    while (notCompleted) {
-
-        if (uptime >= 8.64e+7) {
-
-            days++;
-            uptime -= 8.64e+7;
-
-        } else if (uptime >= 3.6e+6) {
-
-            hours++;
-            uptime -= 3.6e+6;
-
-        } else if (uptime >= 60000) {
-
-            minutes++;
-            uptime -= 60000;
-
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
-
-        }
-
-        if (uptime < 1000)  notCompleted = false;
-
-    }
-
-    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
-
-
-})
-});
-
-
 //THIS MUST BE HERE
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
