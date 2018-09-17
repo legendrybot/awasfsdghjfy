d450582336f6fdde936f15e58975d0d71816baac
legendrybot <42031779+legendrybot@users.noreply.github.com>
2018-09-17 16:50:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "$";
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`I am ready! logged in as ${client.user.tag}`);
 });
 
 client.on('message', message => {
